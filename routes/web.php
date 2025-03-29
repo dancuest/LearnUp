8f7f92bad7bc9071f9eee543877a45ae97f3c0a1
Andres Felipe Cabal Correa <120427118+FelipeCabal@users.noreply.github.com>
2025-03-29 15:27:47 -0500

    Develop/images init (#7)
    * Install s3 dependences
    
    * add(images): upload images to AWS s3
    
    * add buton delete
    
    * end upload images button
    
    * finish button to upload image

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,6 @@ Route::middleware(['auth', 'verified'])->group(function () {
     })->name('dashboard');
 });
 
+require __DIR__ . '/api.php';
 require __DIR__ . '/settings.php';
 require __DIR__ . '/auth.php';
