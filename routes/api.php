7f3e32e2a631fa5a7e65728ea8973b3e38e0ca4c
Felipe Cabal <phelipekabbal_85@hotmail.com>
2025-03-30 15:32:00 -0500

    add(institutions): implementation of institutional management

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ImageController;
+use App\Http\Controllers\Institution\InstitutionController;
+use App\Http\Controllers\Institution\InstitutionUsersController;
 
 Route::prefix('image')->group(function () {
     Route::post('upload', [ImageController::class, 'store'])->name('image.store');
     Route::delete('delete', [ImageController::class, 'destroy'])->name('image.delete');
+});
+
+Route::prefix('institution')->group(function () {
+    Route::get('find/{id}', [InstitutionController::class, 'findById'])->name('institution.findById');
+    Route::get('find', [InstitutionController::class, 'findAll'])->name('institution.findAll');
+
+    Route::middleware('auth')->group(function () {
+        Route::post('create', [InstitutionController::class, 'store'])->name('institution.create');
+        Route::delete('delete', [ImageController::class, 'delete'])->name('image.delete');
+        Route::put('update', [InstitutionController::class, 'update'])->name('institution.update');
+        Route::prefix('user')->group(function () {
+            Route::post('add', [InstitutionUsersController::class, 'addUserToInstitution'])->name('institution.addUser');
+            Route::delete('remove', [InstitutionUsersController::class, 'removeUserFromInstitution'])->name('institution.removeUser');
+        });
+    });
 });
