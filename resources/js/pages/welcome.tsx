4c3cab473372bdc70809857ee488baa4ce2dcdba
Daniel Cuestas <102336853+dancuest@users.noreply.github.com>
2025-03-29 21:13:31 -0500

    welcome correction

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,8 +1,6 @@
 import { type SharedData } from '@/types';
 import { Head, Link, usePage } from '@inertiajs/react';
-import logo from '@../../public/imagenes/Logo.png';
-import background from '@../../public/imagenes/background.png';
-
+import 'swiper/css';
 
 export default function Welcome() {
     const { auth } = usePage<SharedData>().props;
@@ -10,52 +8,44 @@ export default function Welcome() {
     return (
         <>
             <Head title="Welcome">
-                <link rel="preconnect" href="https://fonts.bunny.net" />
-                <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='' />
+                <link href="https://fonts.googleapis.com/css2?family=Archivo+Narrow:ital,wght@0,400..700;1,400..700&family=Roboto+Serif:ital,opsz,wght@0,8..144,100..900;1,8..144,100..900&display=swap" rel="stylesheet" />
             </Head>
-            <div
-                className="flex min-h-screen flex-col items-center bg-contain  bg-center bg-white bg-no-repeat p-6 text-[#1b1b18] lg:justify-center lg:p-8"
-                style={{ backgroundImage: "url('/imagenes/background.png')" }}
-            >                   
-                {/* Logo */}
-                <div className="mb-6 text-center">
-                    <img src="/imagenes/Logo.png" alt="LearnUp Logo" className="w-64 mx-auto" />
+            <div className="flex w-full flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] lg:justify-center lg:p-8 dark:bg-[#0a0a0a]">
+                <header className="mb-6 w-full max-w-[335px] text-sm not-has-[nav]:hidden lg:max-w-4xl">
+                    <nav className="flex items-center justify-center gap-4">
+                        {auth.user ? (
+                            <Link
+                                href={route('dashboard')}
+                                className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
+                            >
+                                Dashboard
+                            </Link>
+                        ) : (
+                            <>
+                                <div className='flex-col justify-evenly items-center'>
 
-                </div>
-                
-                {/* Login Form */}
-                <div className="bg-[#E3ECF6] p-8 rounded-2xl shadow-lg w-96 text-center">
-                    <h2 className="text-xl font-bold mb-4">Iniciar Sesión</h2>
-                    <form className="space-y-4">
-                        <input
-                            type="text"
-                            placeholder="Usuario"
-                            className="w-full p-3 bg-white rounded-4xl border-2 border-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-400"
-                        />
-                        <input
-                            type="password"
-                            placeholder="Contraseña"
-                            className="w-full p-3 bg-white rounded-4xl border-2 border-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-400"
-                        />
-                        <button
-                            type="submit"
-                            className="w-70 bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 transition"
-                        >
-                            Entrar
-                        </button>
-                    </form>
-                    
-                    <button
-                            type="submit"
-                            className="mt-4 w-40 bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 transition"
-                        >
-                            Registrarse
-                        </button>
-                    <div className="mt-4">
-                        <Link href={route('password.request')} className="text-blue-600 hover:underline">Olvidé mi contraseña</Link>
-                    </div>
-                </div>
+                                    <div><Link
+                                        href={route('login')}
+                                        className="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]"
+                                    >
+                                        Log in
+                                    </Link>
+                                        <Link
+                                            href={route('register')}
+                                            className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
+                                        >
+                                            Register
+                                        </Link>
+                                    </div>
+                                </div>
+                            </>
+                        )}
+                    </nav>
+                </header>
             </div>
         </>
     );
 }
+
