79ea628de95018cf308ce4a56f18f4352053831f
Daniel Cuestas <102336853+dancuest@users.noreply.github.com>
2025-04-01 18:55:31 -0500

    Revert "Merge pull request #9 from dancuest/develop/perfil-usuario"
    This reverts commit 3352f0139efa81fa3f01d5c4bdf0e7e464e620ed, reversing
    changes made to 68fa1f5494cf2844f06eeb3b3a441c2c2ff7bbfe.

--- resources/js/layouts/settings/layout.tsx
+++ resources/js/layouts/settings/layout.tsx
@@ -28,11 +28,12 @@ export default function SettingsLayout({ children }: PropsWithChildren) {
     const currentPath = window.location.pathname;
 
     return (
-        <div className="px-4 py-6 grid sm:flex">
-            <div>
-                <Heading title="Settings" description="Manage your profile and account settings" />
+        <div className="px-4 py-6">
+            <Heading title="Settings" description="Manage your profile and account settings" />
+
+            <div className="flex flex-col space-y-8 lg:flex-row lg:space-y-0 lg:space-x-12">
                 <aside className="w-full max-w-xl lg:w-48">
-                    <nav className="space-y-1 space-x-1">
+                    <nav className="flex flex-col space-y-1 space-x-0">
                         {sidebarNavItems.map((item) => (
                             <Button
                                 key={item.url}
@@ -40,7 +41,7 @@ export default function SettingsLayout({ children }: PropsWithChildren) {
                                 variant="ghost"
                                 asChild
                                 className={cn('w-full justify-start', {
-                                    'bg-blue-400': currentPath === item.url,
+                                    'bg-muted': currentPath === item.url,
                                 })}
                             >
                                 <Link href={item.url} prefetch>
@@ -50,11 +51,12 @@ export default function SettingsLayout({ children }: PropsWithChildren) {
                         ))}
                     </nav>
                 </aside>
-                <Separator className="my-6 md:hidden" />
-            </div>
 
-            <div className="border-b-blue-500 rounded-2xl p-6 p-6s shadow-xl flex-1 md:max-w-2xl">
-                <section className="max-w-xl space-y-12">{children}</section>
+                <Separator className="my-6 md:hidden" />
+
+                <div className="flex-1 md:max-w-2xl">
+                    <section className="max-w-xl space-y-12">{children}</section>
+                </div>
             </div>
         </div>
     );
