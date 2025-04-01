5461539669d2d9b6457d502b3e7d44c2f723cbb6
Felipe Cabal <phelipekabbal_85@hotmail.com>
2025-04-01 13:29:02 -0500

    fix institutions services

--- routes/api.php
+++ routes/api.php
@@ -5,18 +5,20 @@ use App\Http\Controllers\ImageController;
 use App\Http\Controllers\Institution\InstitutionController;
 use App\Http\Controllers\Institution\InstitutionUsersController;
 
-Route::prefix('image')->group(function () {
-    Route::post('upload', [ImageController::class, 'store'])->name('image.store');
-    Route::delete('delete', [ImageController::class, 'destroy'])->name('image.delete');
+Route::middleware('auth')->group(function () {
+    Route::prefix('image')->group(function () {
+        Route::post('upload', [ImageController::class, 'store'])->name('image.store');
+        Route::delete('delete', [ImageController::class, 'destroy'])->name('image.delete');
+    });
 });
 
 Route::prefix('institution')->group(function () {
-    Route::get('find/{id}', [InstitutionController::class, 'findById'])->name('institution.findById');
-    Route::get('find', [InstitutionController::class, 'findAll'])->name('institution.findAll');
+    Route::get('', [InstitutionController::class, 'findAll'])->name('institution.findAll');
+    Route::get('{id}', [InstitutionController::class, 'findById'])->name('institution.findById');
 
-    Route::middleware('auth')->group(function () {
-        Route::post('create', [InstitutionController::class, 'store'])->name('institution.create');
-        Route::delete('delete', [ImageController::class, 'delete'])->name('image.delete');
+    Route::middleware(['auth', 'verified'])->group(function () {
+        Route::post('create', [InstitutionController::class, 'create'])->name('institution.create');
+        Route::delete('delete', [InstitutionController::class, 'delete'])->name('institution.delete');
         Route::put('update', [InstitutionController::class, 'update'])->name('institution.update');
         Route::prefix('user')->group(function () {
             Route::post('add', [InstitutionUsersController::class, 'addUserToInstitution'])->name('institution.addUser');
